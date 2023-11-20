06946b216cb2d9c56ba59237cdf913382bad0a69
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-11-20 16:32:23 -0500

    Now saving session data in mongostore

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import passport from "passport";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
@@ -10,10 +11,10 @@ import "dotenv/config";
 // Imported Routes
 import indexRouter from "./routes/index.js";
 
-//Connect to MongoDB 
+// Connect to MongoDB 
 mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "mongo connection error"));
+db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 const app = express();
 
@@ -23,12 +24,22 @@ const __dirname = dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "pug"); // Set up Pug for templating
 
-
-//Render static files
+// Render static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Set up session middleware with a secret and appropriate options
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+// Set up session middleware with a secret and MongoDB store
+app.use(session({
+  secret: process.env.SESSION_SECRET || "cats", 
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ 
+    mongoUrl: process.env.MONGO_URI,
+    collectionName: 'sessions' // Optional, specify a collection name for session data
+  }),
+  cookie: { maxAge: 24 * 60 * 60 * 1000 } // Optional, cookie will expire after 24 hours
+}));
+
+// Set up Passport middleware
 app.use(passport.initialize()); // Initialize Passport
 app.use(passport.session()); // Enable Passport persistent sessions
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
@@ -36,7 +47,7 @@ app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
 // Initialize Routes
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 3000; //Fallback to 3000 for local development
+const PORT = process.env.PORT || 3000;
 
 // Start server on port 3000
-app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
